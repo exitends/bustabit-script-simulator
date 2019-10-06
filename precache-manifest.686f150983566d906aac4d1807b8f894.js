088a18c660e6a7f96fc03759459e68b7779a06e8
Daniel Setzer <dsetzer@enervare.com>
2019-10-06 10:37:02 -0700

    Updates

---
+++ precache-manifest.686f150983566d906aac4d1807b8f894.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "971b82e827d338638c1e47d4a50be646",
+    "url": "/bustabit-script-simulator/index.html"
+  },
+  {
+    "revision": "4e6f6ce574424ee75b33",
+    "url": "/bustabit-script-simulator/static/css/2.b694ccba.chunk.css"
+  },
+  {
+    "revision": "dd1aa5602b78945b8a53",
+    "url": "/bustabit-script-simulator/static/css/main.3d6c64ab.chunk.css"
+  },
+  {
+    "revision": "4e6f6ce574424ee75b33",
+    "url": "/bustabit-script-simulator/static/js/2.197a5e81.chunk.js"
+  },
+  {
+    "revision": "dd1aa5602b78945b8a53",
+    "url": "/bustabit-script-simulator/static/js/main.f37f1f5b.chunk.js"
+  },
+  {
+    "revision": "f82a4ba3138834af119d",
+    "url": "/bustabit-script-simulator/static/js/runtime~main.87d9c740.js"
+  }
+]);
