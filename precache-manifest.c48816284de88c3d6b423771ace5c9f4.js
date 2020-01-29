fa47c2a69efaeeb1b0314a80426a697dd7c75c36
Daniel Setzer <dsetzer@enervare.com>
2020-01-28 20:01:13 -0800

    Updates

---
+++ precache-manifest.c48816284de88c3d6b423771ace5c9f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f723a561e2e2a4d7fdeb56a088bb59a",
+    "url": "/bustabit-script-simulator/index.html"
+  },
+  {
+    "revision": "69503571e9762565ba14",
+    "url": "/bustabit-script-simulator/static/css/2.8c804280.chunk.css"
+  },
+  {
+    "revision": "848f6e328b4d8f187dc3",
+    "url": "/bustabit-script-simulator/static/css/main.72ec65fb.chunk.css"
+  },
+  {
+    "revision": "69503571e9762565ba14",
+    "url": "/bustabit-script-simulator/static/js/2.ddd65ce6.chunk.js"
+  },
+  {
+    "revision": "6ae1564999d323b2d19aca22da8fdfe7",
+    "url": "/bustabit-script-simulator/static/js/2.ddd65ce6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "848f6e328b4d8f187dc3",
+    "url": "/bustabit-script-simulator/static/js/main.db9b443a.chunk.js"
+  },
+  {
+    "revision": "b8ffe4d5845153e34d4c",
+    "url": "/bustabit-script-simulator/static/js/runtime-main.a688cf7f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bustabit-script-simulator/precache-manifest.441240d4145a845b3b22144ae5ab93bf.js"
+  "/bustabit-script-simulator/precache-manifest.c48816284de88c3d6b423771ace5c9f4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bustabit-script-simulator/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
